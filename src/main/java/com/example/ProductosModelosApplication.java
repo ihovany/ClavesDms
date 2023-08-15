b0e3a9e4e3f4633f2ddb9f79eecfcb01f80dd77e
ihovany <130014511+ihovany@users.noreply.github.com>
2023-08-14 22:19:06 -0600

    primer comint

---
+++ src/main/java/com/example/ProductosModelosApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductosModelosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductosModelosApplication.class, args);
+	}
+
+}
